58cb2f2d698eaa963b70634d23507a5aade5b3c9
Ian Hart <ian@ianmadethis.com>
2014-05-29 16:05:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require( 'express' ); 
-var server = express();
+var express = require( 'express' ),
+    server = express();
+
 server.use( express.static(__dirname + '/public/') ); 
 server.listen(3000);
